cc8757d7356baac794cc1206473abe75b71a3570
Joel Ståldal <joelstaldal@Joels-MacBook-Pro-2.local>
2020-06-03 11:54:11 +0200

    Register user ok

--- src/views/Registration.vue
+++ src/views/Registration.vue
@@ -1,12 +1,41 @@
 <template>
     <div class="registration-container">
-        
+        <form @submit.prevent="createUser">
+        <input type="text" v-model="user.email" placeholder="email">
+        <input type="text" v-model="user.password" placeholder="password">
+        <input type="text" v-model="user.repeatPassword" placeholder="repeatPassword">
+        <input type="text" v-model="user.name" placeholder="name">
+        <input type="text" v-model="user.adress" placeholder="adress">
+        <input type="text" v-model="user.street" placeholder="street">
+        <input type="text" v-model="user.city" placeholder="city">
+        <input type="text" v-model="user.zip" placeholder="zip">
+        <input type="submit">
+    </form>
     </div>
 </template>
 
 <script>
 export default {
-name: 'Registration'
+name: 'Registration',
+data() {
+    return {
+        user: {
+            email: "",
+            password: "",
+            repeatPassword: "",
+            name: "",
+            adress: "",
+            street: "",
+            city: "",
+            zip: ""
+        }
+    }
+},
+methods: {
+    createUser() {
+        this.$store.dispatch('', this.user)
+    }
+}
 }
 </script>
 

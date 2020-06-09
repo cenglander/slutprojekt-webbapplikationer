8a4f189b81e7b4507dd71c6e1fad886b7e7eab49
Ivan Jurkovic <icasamoubica@yahoo.co.uk>
2020-06-09 11:10:58 +0200

    mostly finished with basic css

--- src/views/MyAccount.vue
+++ src/views/MyAccount.vue
@@ -1,35 +1,51 @@
 <template>
     <div class="my-account">
         <Header/>
-        <div v-if="getUser">
+        <div class="userPresent" v-if="getUser">
+            <h1>Profile: {{getUser.role}}</h1>
             <div class="profile">
-                <h1>{{getUser.name}}</h1>
-                <h3>{{getUser.role}}</h3>
-                <p>E-mail: {{getUser.email}}</p>
-            </div>
-            <div class="adress">
-                <h3>Adress</h3>
-                <p>Street: {{getUser.adress.street}}</p>
-                <p>ZIP: {{getUser.adress.zip}}</p>
-                <p>City: {{getUser.adress.city}}</p>
+                <div class="profileInfo">
+                    <div>
+                        <p>Name: </p>
+                        <p>{{getUser.name}}</p>
+                    </div>
+                    <div>
+                        <p>E-mail: </p>
+                        <p>{{getUser.email}}</p>
+                    </div>
+                    <div>
+                        <p>Street: </p>
+                        <p>{{getUser.adress.street}}</p>
+                    </div>
+                    <div>
+                        <p>ZIP: </p>
+                        <p>{{getUser.adress.zip}}</p>
+                    </div>
+                    <div>
+                        <p>City: </p>
+                        <p>{{getUser.adress.city}}</p>
+                    </div>
+                </div>
+                <div class="picture">
+                    <img :src="require('@/assets/img/person-circle-outline.svg')" alt="">
+                </div>
             </div>
             <div class="orders">
-                <p>_____________________________________</p>
-                <h3>Orders</h3>
-                <ul>
+                <h1>Order History</h1>
+                <button :click="refreashOrders">Refreash</button>
+                <ul class="listOfOrders">
                     <li v-for="singleOrder in orders" 
                         :key="singleOrder._id">
-                        <p>_____________________________________</p>
-                        <p>Time: {{singleOrder.timeStamp}}</p>
-                        <p>Status: {{singleOrder.status}}</p>
-                        <p>Value: {{singleOrder.orderValue}}</p>
+                        <p>{{getDate(singleOrder.timeStamp)}}</p>
+                        <p>{{getTime(singleOrder.timeStamp)}}</p>
+                        <p>{{singleOrder.status}}</p>
+                        <p>{{singleOrder.orderValue}} kr</p>
                         <p>Items: {{singleOrder.items.length}}</p>
-                        <p>_____________________________________</p>
                     </li>
                 </ul>
             </div>
         </div>
-        <div v-else>
+        <div class="noUser" v-else>
             <h1>Please log in</h1>
         </div>
     </div>
@@ -49,11 +65,36 @@ export default {
         Header
     },
 
+    methods: {
+        async refreashOrders() {
+            this.orders = await this.$store.dispatch('getAllOrders')
+        },
+        getTime(timeStamp) {
+            var date = new Date(timeStamp)
+            var hours = date.getHours()
+            var minutes = "0" + date.getMinutes()
+            var seconds = "0" + date.getSeconds()
+            var formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+            return formattedTime
+        },
+        getDate(timeStamp) {
+            var date = new Date(timeStamp)
+            var day = date.getDay()
+            if (day<10) {
+                day = `0` + day
+            }
+            var month = date.getMonth()
+            if (month<10) {
+                month = `0` + month
+            }
+            var year = date.getFullYear()
+            return year + `-` + month + `-` + day
+        }
+
+    },
+
     computed: {
         getUser() {
-            console.log('getting user from store');
-            console.log(this.$store.state.currentUser);
-            
             return this.$store.state.currentUser
         },
     },
@@ -62,12 +103,100 @@ export default {
         this.$store.commit('restoreSession')
         this.orders = await this.$store.dispatch('getAllOrders')
     }
-
-
 }
 </script>
 
 <style scoped lang="scss">
+
+.my-account {
+    margin: auto;
+    background-color: #F0F0F0;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    max-width: 1000px;
+
+    .userPresent{
+
+        .profile {
+
+            background-color: #EBEBEB;
+            min-width: 40rem;
+            max-width: 700px;
+            margin: 0;
+            padding-top: 3.5rem;
+            padding-bottom: 3.5rem;
+            border-radius: 2rem;
+            margin-top: 3rem;
+            margin-bottom: 3rem;
+            display: grid;
+            grid-template-columns: 2fr 1fr;
+
+            .profileInfo {
+
+                div:nth-child(even) {
+                    background-color: white;
+                }
+
+                div {
+                    height: 3rem;
+                    margin: 0;
+                    display: grid;
+                    align-items: center;
+                    margin-left: 3rem;
+                    grid-template-columns: 2fr 4fr;
+                    max-width: 22rem;
+                    background-color: #F1F1F1;
+
+                }
+            }
+            .picture {
+
+                margin: auto;
+
+                img {
+                    height: 11rem;
+                    widows: 11rem;
+                    position: center;
+                }
+            }
+        }  
+
+        .orders {
+
+            .listOfOrders {
+
+                background-color: #EBEBEB;
+                min-width: 40rem;
+                max-width: 700px;
+                margin: 0;
+                padding-top: 3.5rem;
+                padding-bottom: 3.5rem;
+                border-radius: 2rem;
+                margin-top: 3rem;
+                margin-bottom: 3rem;
+
+                li:nth-child(even) {
+                    background-color: white;
+                }
+
+                li {
+                    background-color: #F1F1F1;
+                    margin: 0;
+                    padding: 0;
+                    max-width: 38rem;
+                    list-style: none;
+                    display: grid;
+                    grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+                }
+            }
+        }
+    }
+}
+
+
+
+
 li {
     list-style: none;
 }
